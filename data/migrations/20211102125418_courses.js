3e354c86341df3278255fa994fae6409eb10810c
WillisLi <li.willis95@gmail.com>
2021-11-02 14:28:59 -0700

    We created the migration tables for the database schema.
    Co-authored-by: Amanuel Gilay <emahawey27@gmail.com>
    Co-authored-by: Andrew Cummings <andrew.amalee@gmail.com>

---
+++ data/migrations/20211102125418_courses.js
@@ -0,0 +1,34 @@
+exports.up = (knex) => {
+  return knex.schema
+    .createTable('courses', function (table) {
+      table.increments('id');
+      table.integer('size').notNullable();
+      table.text('description').notNullable();
+      table.string('subject').notNullable();
+      table.integer('prereq');
+    })
+
+    .createTable('schedules', (table) => {
+      table.increments('id');
+      table.time('start_time').notNullable();
+      table.time('end_time').notNullable();
+      table.date('start_date').notNullable();
+      table.date('end_date').notNullable();
+      table.string('location').notNullable();
+
+      table
+        .integer('course_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('courses')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('schedules')
+    .dropTableIfExists('courses');
+};
