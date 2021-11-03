5f390ca0b21df5189c93d5247178d407768e5f71
WillisLi <42555076+WillisLi@users.noreply.github.com>
2021-11-03 16:19:55 -0700

    Rechecked and changed dummy data.

--- data/seeds/002_parents.js
+++ data/seeds/002_parents.js
@@ -1,31 +1,13 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('table_name')
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('parents').insert([
         {
-          first_name: 'David',
-          middle_initial: 'W',
-          last_name: 'Crosland',
-          county: 'Baltimore',
-          image_url: faker.image.avatar(),
+          user_id: 1,
         },
         {
-          first_name: 'Patricia',
-          middle_initial: 'A',
-          last_name: 'Cole',
-          county: 'Virginia Falls',
-          image_url: faker.image.avatar(),
+          user_id: 2,
         },
         {
-          first_name: 'Neil',
-          middle_initial: 'P',
-          last_name: 'Miller',
-          county: 'Virginia Falls',
-          image_url: faker.image.avatar(),
+          user_id: 3,
         },
       ]);
-    });
 };
