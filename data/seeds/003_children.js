f341f08ffabcd19a76acdd86741977764447dfa5
WillisLi <42555076+WillisLi@users.noreply.github.com>
2021-11-03 16:06:48 -0700

    Changed table_name to "children"

--- data/seeds/003_children.js
+++ data/seeds/003_children.js
@@ -1,13 +1,7 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('table_name')
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        { parent_id: 2, user_id: 5, username: 'FidgetSpinnersRLyfe', age: 12 },
-        { parent_id: 5, user_id: 20, username: 'FortniteGamerBoy', age: 13 },
-        { parent_id: 3, user_id: 34, username: 'VapeGod360XXX', age: 11 },
+      return knex('children').insert([
+        { parent_id: 2, user_id: 5, username: 'ILoveFortnite', age: 12 },
+        { parent_id: 5, user_id: 2, username: 'BananaBread', age: 13 },
+        { parent_id: 3, user_id: 4, username: 'AwesomeDinosaur', age: 11 },
       ]);
-    });
 };
