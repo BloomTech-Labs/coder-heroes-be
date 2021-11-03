f6e1ccc4a7b01a0996dfe9474c34eb81f662d98d
WillisLi <42555076+WillisLi@users.noreply.github.com>
2021-11-03 16:02:47 -0700

    Cleaned up instructor_list

--- data/seeds/011_instructor_list.js
+++ data/seeds/011_instructor_list.js
@@ -1,13 +1,7 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('instructor_list')
-    .del()
-    .then(function () {
-      // Inserts seed entries
       return knex('instructor_list').insert([
-        { instructor_id: 2, course_id: 5 },
-        { instructor_id: 6, course_id: 3 },
+        { instructor_id: 2, course_id: 4 },
+        { instructor_id: 4, course_id: 3 },
         { instructor_id: 1, course_id: 2 },
       ]);
-    });
 };
