c1fd37717bcdf733ec54d9a1fbdf6c395227c8b1
WillisLi <li.willis95@gmail.com>
2021-11-10 13:59:47 -0800

    debugged endpoints and implemented enrollments to children router

--- api/children/childrenModel.js
+++ api/children/childrenModel.js
@@ -4,26 +4,65 @@ const getChildren = async () => {
   return await db('children');
 };
 
-const findById = async (id) => {
-  return db('children').where({ id });
+const findByChildId = async (id) => {
+  return db('children')
+    .leftJoin('profiles', 'children.user_id', 'profiles.key')
+    .where('children.id', id);
 };
 
-const addChildren = async (children) => {
-  return db('children').insert(children).returning('*');
+const findByUserId = async (id) => {
+  return db('children')
+    .leftJoin('profiles', 'children.user_id', 'profiles.key')
+    .where('children.user_id', id);
 };
 
-const removeChildren = async (children) => {
-  return await db('children').where({ children }).del();
+const checkEnrolled = async (child_id, schedule_id) => {
+  return await db('enrollments')
+    .where('child_id', child_id)
+    .where('schedule_id', schedule_id);
 };
 
-const updateChildren = (id, children) => {
-  return db('children').where({ id }).first().update(children).returning('*');
+const checkEnrolledExists = async (id) => {
+  return await db('enrollments').where({ id });
+};
+
+const getEnrolledCourses = async (id) => {
+  return await db('children')
+    .leftJoin('enrollments', 'children.id', 'enrollments.child_id')
+    .leftJoin('schedules', 'enrollments.schedule_id', 'schedules.id')
+    .where('children.id', id);
+};
+
+const addEnrolledCourse = async (course) => {
+  return await db('enrollments').insert(course).returning('*');
+};
+
+const addChild = async (child) => {
+  return db('children').insert(child).returning('*');
+};
+
+const updateChild = async (id, child) => {
+  return await db('children').where({ id }).update(child).returning('*');
+};
+
+const removeChild = async (id) => {
+  return await db('children').where({ id }).del();
+};
+
+const removeCourse = async (id) => {
+  return await db('enrollments').where({ id }).del();
 };
 
 module.exports = {
   getChildren,
-  findById,
-  addChildren,
-  removeChildren,
-  updateChildren,
+  getEnrolledCourses,
+  addEnrolledCourse,
+  checkEnrolledExists,
+  checkEnrolled,
+  findByChildId,
+  findByUserId,
+  addChild,
+  removeCourse,
+  updateChild,
+  removeChild,
 };
--- api/children/childrenRouter.js
+++ api/children/childrenRouter.js
@@ -15,13 +15,13 @@ router.get('/', function (req, res) {
 
 router.get('/:id', function (req, res) {
   const id = req.params.id;
-  Children.findById(id)
+  Children.findByChildId(id)
     .then((child) => {
       if (child && Object.keys(child).length !== 0) {
         //need to return this way because the array will always return something and never not exist
         res.status(200).json(child);
       } else {
-        res.status(404).json({ error: 'Student not found' });
+        res.status(404).json({ error: 'Child not found' });
       }
     })
     .catch((err) => {
@@ -29,22 +29,66 @@ router.get('/:id', function (req, res) {
     });
 });
 
+router.get('/:id/enrollments', function (req, res) {
+  const { id } = req.params;
+  Children.getEnrolledCourses(id)
+    .then((schedules) => {
+      if (schedules) {
+        res.status(200).json(schedules);
+      } else {
+        res.status(404).json({ error: 'ChildSchedulesNotFound' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.post('/:id/enrollments', async (req, res) => {
+  const course = req.body;
+  if (course) {
+    const { child_id, schedule_id } = course;
+    try {
+      await Children.checkEnrolled(child_id, schedule_id).then(
+        async (exists) => {
+          if (exists.length === 0) {
+            await Children.addEnrolledCourse(course).then((added) =>
+              res.status(200).json({
+                message: 'Student was enrolled!',
+                course: added[0],
+              })
+            );
+          } else {
+            res
+              .status(400)
+              .json({ message: 'This child is enrolled in the course.' });
+          }
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(404).json({ message: 'Enrollment details missing.' });
+  }
+});
+
 router.post('/', async (req, res) => {
   const child = req.body;
-  console.log(child);
   if (child) {
-    const id = child.id;
+    const { user_id } = child;
     try {
-      await Children.findById(id).then(async (exists) => {
+      await Children.findByUserId(user_id).then(async (exists) => {
         if (exists.length === 0) {
-          await Children.addChildren(child).then((added) =>
+          await Children.addChild(child).then((added) =>
             res.status(200).json({
               message: 'Student was created successfully!',
-              course: added[0],
+              child: added[0],
             })
           );
         } else {
-          res.status(400).json({ message: 'this student already exists' });
+          res.status(400).json({ message: 'This child already exists.' });
         }
       });
     } catch (err) {
@@ -52,12 +96,75 @@ router.post('/', async (req, res) => {
       res.status(500).json({ message: err.message });
     }
   } else {
-    res.status(404).json({ message: 'Student Details missing ' });
+    res.status(404).json({ message: 'Child Details missing.' });
   }
 });
 
-// router.put('/:id', (req, res) => {
-//   console.log('is it secret? is it safe?');
-// });
+router.put('/', (req, res) => {
+  const child = req.body;
+  if (child) {
+    const { id } = child;
+    Children.findByChildId(id)
+      .then(
+        Children.updateChild(id, child)
+          .then((updated) => {
+            res.status(200).json({
+              message: `Child with id: ${id} updated`,
+              child: updated[0],
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              message: `Could not update child '${id}'`,
+              error: err.message,
+            });
+          })
+      )
+      .catch((err) => {
+        res.status(404).json({
+          message: `Could not find child '${id}'`,
+          error: err.message,
+        });
+      });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  try {
+    Children.findByChildId(id).then((child) => {
+      Children.removeChild(child[0].id).then(() => {
+        res.status(200).json({
+          message: `Child with id:'${id}' was deleted.`,
+          child: child[0],
+        });
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not delete child with ID: ${id}`,
+      error: err.message,
+    });
+  }
+});
+
+router.delete('/enrollments/:id', function (req, res) {
+  const { id } = req.params;
+  try {
+    Children.checkEnrolledExists(id).then((course) => {
+      Children.removeCourse(course[0].id).then(() => {
+        res.status(200).json({
+          message: `Enrolled Course with id:'${id}' was deleted from child_id: ${course[0].child_id}'s' enrollments list.`,
+          course: course[0],
+        });
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: `Could not delete course with ID: ${id}`,
+      error: err.message,
+    });
+  }
+});
 
 module.exports = router;
