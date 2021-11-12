d06a95bbf91668d44ca7a33623e5702c36e318cf
WillisLi <li.willis95@gmail.com>
2021-11-12 08:23:49 -0800

    created user endpoints
    Co-authored-by: Andrew Cummings <andrew.amalee@gmail.com>

--- api/user/userModel.js
+++ api/user/userModel.js
@@ -1,15 +1,38 @@
 const db = require('../../data/db-config');
 
 const findUserData = async (type, id) => {
-  if (type === '1') {
+  if (type === 1) {
     return db('profiles').leftJoin('admins', `${id}`, 'admins.user_id');
-  } else if (type === '2') {
-    return db('profiles').leftJoin('admins', `${id}`, 'admins.user_id');
+  } else if (type === 2) {
+    return db('profiles').leftJoin('parents', `${id}`, 'parents.user_id');
+  } else if (type === 3) {
+    return db('profiles').leftJoin(
+      'instructors',
+      `${id}`,
+      'instructors.user_id'
+    );
+  } else if (type === 4) {
+    return db('profiles').leftJoin('children', `${id}`, 'children.user_id');
   }
 };
 
+const getInbox = (okta) => {
+  return db('profiles')
+    .leftJoin('inboxes', 'profiles.okta', 'inboxes.user_id')
+    .leftJoin('messages', 'inboxes.id', 'messages.inbox_id')
+    .where({ okta });
+};
+
+const getSchedule = (okta) => {
+  return db('profiles')
+    .leftJoin('parents', 'profiles.okta', 'parents.user_id')
+    .leftJoin('children', 'parents.id', 'children.parent_id')
+    .leftJoin('enrollments', 'children.id', 'enrollments.child_id')
+    .leftJoin('schedules', 'enrollments.schedule_id', 'schedules.id')
+    .where('profiles.okta', okta);
+};
+
 const updateUserData = (id, profile) => {
-  console.log(profile);
   return db('profiles')
     .where({ id: id })
     .first()
@@ -19,5 +42,7 @@ const updateUserData = (id, profile) => {
 
 module.exports = {
   findUserData,
+  getInbox,
+  getSchedule,
   updateUserData,
 };
--- api/user/userRouter.js
+++ api/user/userRouter.js
@@ -19,4 +19,32 @@ router.get('/', authRequired, function (req, res) {
   });
 });
 
+router.get('/inbox', authRequired, function (req, res) {
+  const { okta } = req.profile;
+  Profiles.findById(okta).then((user) => {
+    User.getInbox(user.okta)
+      .then((inbox) => {
+        res.status(200).json(inbox);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+      });
+  });
+});
+
+router.get('/schedules', authRequired, function (req, res) {
+  const { okta } = req.profile;
+  Profiles.findById(okta).then((user) => {
+    User.getSchedule(user.okta)
+      .then((schedule) => {
+        res.status(200).json(schedule);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+      });
+  });
+});
+
 module.exports = router;
