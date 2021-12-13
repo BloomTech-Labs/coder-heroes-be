ec450911b7801167cbbba738a59f921185d537ce
justinfineberg <justinf1818@gmail.com>
2021-12-13 13:41:30 -0500

    updated adminModel with new schema

--- api/admin/adminModel.js
+++ api/admin/adminModel.js
@@ -3,21 +3,21 @@ const db = require('../../data/db-config');
 const getAdmins = async () => {
   return await db('admins').leftJoin(
     'profiles',
-    'admins.user_id',
-    'profiles.okta'
+    'admins.admin_id',
+    'profiles.okta_id'
   );
 };
 
 const findByAdminId = async (id) => {
   return db('admins')
-    .leftJoin('profiles', 'admins.user_id', 'profiles.okta')
-    .where('admins.id', id);
+    .leftJoin('profiles', 'admins.admin_id', 'profiles.okta_id')
+    .where('admins.admin_id', id);
 };
 
 const findByOkta = async (okta) => {
   return db('admins')
-    .leftJoin('profiles', 'admins.user_id', 'profiles.okta')
-    .where('admins.user_id', okta);
+    .leftJoin('profiles', 'admins.admin_id', 'profiles.okta_id')
+    .where('admins.admin_id', okta);
 };
 
 const addAdmin = async (admin) => {
@@ -25,11 +25,11 @@ const addAdmin = async (admin) => {
 };
 
 const updateAdmin = async (id, admin) => {
-  return await db('admins').where({ id }).update(admin);
+  return await db('admins').where('admin_id', id).update(admin);
 };
 
 const removeAdmin = async (id) => {
-  return await db('admins').where({ id }).del();
+  return await db('admins').where('admin_id', id).del();
 };
 
 module.exports = {
