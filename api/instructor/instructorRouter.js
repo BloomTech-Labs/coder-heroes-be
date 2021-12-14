5fafab6d06fc834ffe2b0e7894d29efa8d168287
Steven J. Homem <sjhomem@outlook.com>
2021-12-14 16:01:05 -0600

    updated the model functions for the instructor route and also updated the router as well where I could, we still need to test these router endpoints with our seed data when that gets implemented

--- api/instructor/instructorRouter.js
+++ api/instructor/instructorRouter.js
@@ -14,14 +14,14 @@ router.get('/', authRequired, function (req, res) {
     });
 });
 
-router.get('/:id', authRequired, function (req, res) {
-  const id = String(req.params.id);
-  Instructors.findByInstructorId(id)
+router.get('/:instructor_id', authRequired, function (req, res) {
+  const instructor_id = String(req.params.instructor_id);
+  Instructors.findByInstructorId(instructor_id)
     .then((instructor) => {
       if (instructor) {
         res.status(200).json(instructor);
       } else {
-        res.status(404).json({ error: 'InstructorNotFound' });
+        res.status(404).json({ error: 'Instructor Not Found' });
       }
     })
     .catch((err) => {
@@ -29,14 +29,14 @@ router.get('/:id', authRequired, function (req, res) {
     });
 });
 
-router.get('/:id/courses', authRequired, function (req, res) {
-  const id = String(req.params.id);
-  Instructors.findInstructorCourses(id)
-    .then((courses) => {
-      if (courses) {
-        res.status(200).json(courses);
+router.get('/:instructor_id/classes', authRequired, function (req, res) {
+  const instructor_id = String(req.params.instructor_id);
+  Instructors.findInstructorCourses(instructor_id)
+    .then((classes) => {
+      if (classes) {
+        res.status(200).json(classes);
       } else {
-        res.status(404).json({ error: 'InstructorCoursesNotFound' });
+        res.status(404).json({ error: 'Instructor classes Not Found' });
       }
     })
     .catch((err) => {
@@ -47,9 +47,9 @@ router.get('/:id/courses', authRequired, function (req, res) {
 router.post('/', async (req, res) => {
   const instructor = req.body;
   if (instructor) {
-    const { user_id } = instructor;
+    const { profile_id } = instructor;
     try {
-      await Instructors.findByOkta(user_id).then(async (user) => {
+      await Instructors.findByProfileId(profile_id).then(async (user) => {
         if (user.length === 0) {
           await Instructors.addInstructor(instructor).then((inserted) =>
             res
@@ -72,46 +72,46 @@ router.post('/', async (req, res) => {
 router.put('/', authRequired, (req, res) => {
   const instructor = req.body;
   if (instructor) {
-    const { id } = instructor;
-    Instructors.findByInstructorId(id)
+    const { instructor_id } = instructor;
+    Instructors.findByInstructorId(instructor_id)
       .then(
-        Instructors.updateInstructor(id, instructor)
+        Instructors.updateInstructor(instructor_id, instructor)
           .then((updated) => {
             res.status(200).json({
-              message: `Instructor with id: ${id} updated`,
+              message: `Instructor with id: ${instructor_id} updated`,
               instructor: updated[0],
             });
           })
           .catch((err) => {
             res.status(500).json({
-              message: `Could not update instructor '${id}'`,
+              message: `Could not update instructor '${instructor_id}'`,
               error: err.message,
             });
           })
       )
       .catch((err) => {
         res.status(404).json({
-          message: `Could not find instructor '${id}'`,
+          message: `Could not find instructor '${instructor_id}'`,
           error: err.message,
         });
       });
   }
 });
 
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
+router.delete('/:instructor_id', (req, res) => {
+  const instructor_id = req.params.instructor_id;
   try {
-    Instructors.findByInstructorId(id).then((instructor) => {
-      Instructors.removeInstructor(instructor[0].id).then(() => {
+    Instructors.findByInstructorId(instructor_id).then((instructor) => {
+      Instructors.removeInstructor(instructor[0].instructor_id).then(() => {
         res.status(200).json({
-          message: `Instructor with id:'${id}' was deleted.`,
+          message: `Instructor with id:'${instructor_id}' was deleted.`,
           instructor: instructor[0],
         });
       });
     });
   } catch (err) {
     res.status(500).json({
-      message: `Could not delete instructor with ID: ${id}`,
+      message: `Could not delete instructor with ID: ${instructor_id}`,
       error: err.message,
     });
   }
