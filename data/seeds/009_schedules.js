83f5f18beb511afeb96d39e191d99b06089ca56b
Ashley Burns <Ash_burn999@yahoo.com>
2021-12-14 20:57:20 -0500

    added sessions.js to schedules.js aka (classes table) and reformated seeds

--- data/seeds/009_schedules.js
+++ data/seeds/009_schedules.js
@@ -1,19 +1,26 @@
 exports.seed = function (knex) {
-  return knex('schedules').insert([
+  return knex('classes').insert([
     {
       size: 15,
-      course_id: 1,
+      open_seats_remaining: 5,
+      course_type_id: 1,
       instructor_id: 1,
+      start_time: '5:00pm',
+      start_date: '10-10-2010',
+      end_date: '10-10-2010',
+      end_time: '6:00pm',
+      location: 'https://zoom.us/my/john123',
     },
     {
-      size: 12,
-      course_id: 2,
+      size: 10,
+      open_seats_remaining: 10,
+      course_type_id: 2,
       instructor_id: 2,
-    },
-    {
-      size: 1,
-      course_id: 3,
-      instructor_id: 3,
+      start_time: '5:00pm',
+      start_date: '10-10-2010',
+      end_date: '10-10-2010',
+      end_time: '6:00pm',
+      location: 'https://zoom.us/my/john123',
     },
   ]);
 };
