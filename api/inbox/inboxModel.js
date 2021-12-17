17a1c80b3a9d8869dc95789afebacc8f5347a734
Steven J. Homem <sjhomem@outlook.com>
2021-12-17 12:42:14 -0600

    we had discussed gutting inbox router and model because it was built without pairing with the frontend. we think it will be best built when the application is needing an inbox. the engineers who will be working on this feature will be the best to decide how it needs to be built. we can only guess and we do not want to cause grief for future engineers

--- api/inbox/inboxModel.js
+++ api/inbox/inboxModel.js
@@ -1,43 +1,39 @@
-const db = require('../../data/db-config');
+// const db = require('../../data/db-config');
 
-const getInboxes = async () => {
-  return await db('inboxes')
-    .leftJoin('profiles', 'inboxes.profile_id', '=', 'profiles.okta')
-    .innerJoin('messages', 'inboxes.inbox_id', '=', 'messages.inbox_id');
-};
+// const getInboxes = async () => {
+//   return await }
+// };
 
-const findByProfileId = async (profile_id) => {
-  return db('inboxes')
-    .innerJoin('messages', 'inboxes.inbox_id', '=', 'messages.inbox_id')
-    .where({ profile_id });
-};
+// const findByProfileId = async (profile_id) => {
+//   return await
+// };
 
-const findByInboxId = async (inbox_id) => {
-  return db('inboxes').where('id', inbox_id);
-};
+// const findByInboxId = async (inbox_id) => {
+//   return await
+// };
 
-const addInbox = async (inbox) => {
-  return db('inboxes').insert(inbox).returning('*');
-};
+// const addInbox = async (inbox) => {
+//   return await
+// };
 
-const addMessage = async (message) => {
-  return db('messages').insert(message).returning('*');
-};
+// const addMessage = async (message) => {
+//   return await
+// };
 
-const updateInbox = (profile_id, inbox) => {
-  return db('inboxes').where({ profile_id }).update(inbox).returning('*');
-};
+// const updateInbox = (profile_id, inbox) => {
+//   return await
+// };
 
-const removeInbox = async (profile_id) => {
-  return await db('inboxes').where({ profile_id }).del();
-};
+// const removeInbox = async (profile_id) => {
+//   return await
+// };
 
-module.exports = {
-  getInboxes,
-  findByProfileId,
-  findByInboxId,
-  addInbox,
-  addMessage,
-  updateInbox,
-  removeInbox,
-};
+// module.exports = {
+//   getInboxes,
+//   findByProfileId,
+//   findByInboxId,
+//   addInbox,
+//   addMessage,
+//   updateInbox,
+//   removeInbox,
+// };
--- api/inbox/inboxRouter.js
+++ api/inbox/inboxRouter.js
@@ -1,131 +1,30 @@
-const express = require('express');
-const Inboxes = require('./inboxModel');
-const router = express.Router();
-const authRequired = require('../middleware/authRequired');
+// const express = require('express');
+// const Inboxes = require('./inboxModel');
+// const router = express.Router();
+// const authRequired = require('../middleware/authRequired');
 
-router.get('/', authRequired, function (req, res) {
-  Inboxes.getInboxes()
-    .then((inboxList) => {
-      res.status(200).json(inboxList);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: err.message });
-    });
-});
+// router.get('/', authRequired, function (req, res) {
 
-router.get('/:profile_id', authRequired, function (req, res) {
-  const profile_id = req.params.profile_id;
-  Inboxes.findByProfileId(profile_id)
-    .then((inbox) => {
-      if (inbox && Object.keys(inbox).length !== 0) {
-        res.status(200).json(inbox);
-      } else {
-        res.status(404).json({ error: 'InboxNotFound' });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
+// });
 
-router.post('/', async (req, res) => {
-  const inbox = req.body;
-  if (inbox) {
-    const { profile_id } = inbox;
-    try {
-      await Inboxes.findByProfileId(profile_id).then(async (exists) => {
-        if (exists.length === 0) {
-          await Inboxes.addInbox(inbox).then((inserted) =>
-            res.status(200).json({
-              message: 'Inbox created successfully!',
-              inbox: inserted[0],
-            })
-          );
-        } else {
-          res
-            .status(400)
-            .json({ message: 'Inbox already exists for this user.' });
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ message: e.message });
-    }
-  } else {
-    res.status(404).json({ message: 'Inbox details missing.' });
-  }
-});
+// router.get('/:profile_id', authRequired, function (req, res) {
 
-router.post('/messages', authRequired, async (req, res) => {
-  const message = req.body;
-  if (message) {
-    const { inbox_id } = message;
-    try {
-      await Inboxes.findByInboxId(inbox_id).then(async (exists) => {
-        if (exists.length != 0) {
-          await Inboxes.addMessage(message).then((inserted) =>
-            res.status(200).json({
-              message: 'Message sent successfully!',
-              sent: inserted[0],
-            })
-          );
-        } else {
-          res.status(400).json({ message: 'Inbox does not exist.' });
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ message: e.message });
-    }
-  } else {
-    res.status(404).json({ message: 'Message details missing.' });
-  }
-});
+// });
 
-router.put('/', authRequired, (req, res) => {
-  const { profile_id } = req.body;
-  Inboxes.findByProfileId(profile_id)
-    .then((inbox) => {
-      Inboxes.updateInbox(inbox[0].profile_id, req.body)
-        .then((updated) => {
-          res.status(200).json({
-            message: 'Inbox updated successfully.',
-            inbox: updated[0],
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            message: `Could not update inbox: '${profile_id}'.`,
-            error: err.message,
-          });
-        });
-    })
-    .catch((err) => {
-      res.status(404).json({
-        message: `Could not find user: '${profile_id}'`,
-        error: err.message,
-      });
-    });
-});
+// router.post('/', async (req, res) => {
 
-router.delete('/:profile_id', (req, res) => {
-  const profile_id = req.params.profile_id;
-  try {
-    Inboxes.findByProfileId(profile_id).then((inbox) => {
-      Inboxes.removeInbox(inbox[0].profile_id).then(() => {
-        res.status(200).json({
-          message: `Inbox with profile_id: '${inbox[0].profile_id}' was deleted.`,
-          inbox: inbox,
-        });
-      });
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: `Could not delete inbox with profile_id: ${profile_id}.`,
-      error: err.message,
-    });
-  }
-});
+// });
 
-module.exports = router;
+// router.post('/messages', authRequired, async (req, res) => {
+
+// });
+
+// router.put('/', authRequired, (req, res) => {
+
+// });
+
+// router.delete('/:profile_id', (req, res) => {
+
+// });
+
+// module.exports = router;
