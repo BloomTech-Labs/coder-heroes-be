3f084806bdda9cd118563f2334830190470ac0ee
zach-morris-txt <zach-morris@lambdastudents.com>
2021-12-20 00:03:41 -0500

    Handling ESlint requirements

--- api/payment/stripeRouter.js
+++ api/payment/stripeRouter.js
@@ -1,21 +1,21 @@
-const stripe = require('stripe')(process.env.STRIPE_KEY)
-const router = require('express').Router()
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
+// const router = require('express').Router();
 
-router.post("/payment", (req, res, next) => {
-    stripe.charges.create({
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "usd",
-    }, (stripeErr, stripeRes) => {
-        if(stripeErr) {
-            res.status(500).json(stripeErr)
-        } else {
-            res.status(200).json(stripeRes)
-        }
-    })
-})
+// router.post('/payment', (req, res) => {
+//     stripe.charges.create({
+//         source: req.body.tokenId,
+//         amount: req.body.amount,
+//         currency: 'usd',
+//     }, (stripeErr, stripeRes) => {
+//         if(stripeErr) {
+//             res.status(500).json(stripeErr)
+//         } else {
+//             res.status(200).json(stripeRes)
+//         }
+//     })
+// })
 
-module.exports = router
+// module.exports = router
 
 //IMPLEMENTATION:
 //npm i stripe   ***not "stripe-checkout" but "stripe"*** 
