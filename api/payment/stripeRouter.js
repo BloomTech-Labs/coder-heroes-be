3e75902e9797ce898112c461d30be56517a7b803
zach-morris-txt <zach-morris@lambdastudents.com>
2021-12-19 23:54:50 -0500

    Payment w/ Stripe; router

---
+++ api/payment/stripeRouter.js
@@ -0,0 +1,24 @@
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+const router = require('express').Router()
+
+router.post("/payment", (req, res, next) => {
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "usd",
+    }, (stripeErr, stripeRes) => {
+        if(stripeErr) {
+            res.status(500).json(stripeErr)
+        } else {
+            res.status(200).json(stripeRes)
+        }
+    })
+})
+
+module.exports = router
+
+//IMPLEMENTATION:
+//npm i stripe   ***not "stripe-checkout" but "stripe"*** 
+//import router into index
+//server router
+//.env file --> "STRIPE_KEY=" --> stripe test account public key
