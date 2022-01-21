577fecff451aaff7923fb6fd405f288b7f53d802
Daniel Reinagel <daniel.e.reinagel@gmail.com>
2022-01-21 13:37:19 -0700

    changed a const to a let so that the variables could be changed without errors

--- api/courseTypes/courseTypesMiddleware.js
+++ api/courseTypes/courseTypesMiddleware.js
@@ -13,15 +13,29 @@ const checkIfCourseIsUnique = async (req, res, next) => {
   }
 };
 const checkCoursePyload = (req, res, next) => {
-  const { description, subject } = req.body;
-  if (description.trim() && subject.trim()) {
-    next();
-  } else {
+  let { description, subject } = req.body;
+  if (typeof description !== 'string')
+    next({ status: 400, message: 'description must be of type string' });
+  if (typeof subject !== 'string')
+    next({ status: 400, message: 'subject must be of type string' });
+  description = description.trim();
+  subject = subject.trim();
+  req.body = { description, subject };
+  if (!description) next({ status: 400, message: 'description is required' });
+  if (!subject) next({ status: 400, message: 'subject is required' });
+  if (description.length > 255)
     next({
-      status: 404,
-      message: 'please complete description and subject section.',
+      status: 400,
+      message:
+        'the description string must not exceed a length of 255 characters',
     });
-  }
+  if (subject.length > 255)
+    next({
+      status: 400,
+      message: 'the subject string must not exceed a length of 255 characters',
+    });
+
+  next();
 };
 
 module.exports = {
