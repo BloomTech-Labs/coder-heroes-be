8e251e28f13ab7e46655eadc3a3b7f1cb099fa2f
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2022-01-26 19:30:03 -0500

    removes incorrect data from seed that would not let seed run

--- data/seeds/015_instructor_approved_course_types.js
+++ data/seeds/015_instructor_approved_course_types.js
@@ -62,12 +62,12 @@ exports.seed = function (knex) {
       course_type_id: 3,
     },
     {
-      profile_id: 3,
-      course_type_id: 4,
+      profile_id: 8,
+      course_type_id: 1,
     },
     {
       profile_id: 8,
-      course_type_id: 1,
+      course_type_id: 4,
     },
     {
       profile_id: 8,
@@ -78,9 +78,6 @@ exports.seed = function (knex) {
       course_type_id: 6,
     },
     {
-      profile_id: 8,
-      course_type_id: 7,
-    },{
       profile_id: 2,
       course_type_id: 1,
     },
