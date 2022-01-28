e9da535ee856497e64b69575cd3904b3866f3742
Daniel Reinagel <daniel.e.reinagel@gmail.com>
2022-01-28 12:58:07 -0700

    fixed instructorMiddleware checkInstructorObject where I was passing in a profile_id in FindById (it needs an okta_id)

--- api/instructor/instructorMiddleware.js
+++ api/instructor/instructorMiddleware.js
@@ -1,7 +1,7 @@
 const Instructors = require('./instructorModel');
 
 const { findByAdminId } = require('../admin/adminModel');
-const { findById } = require('../profile/profileModel');
+const { findBy } = require('../profile/profileModel');
 
 const checkInstructorExist = (fromParams) => async (req, res, next) => {
   const instructor_id = req[fromParams ? 'params' : 'body'].instructor_id;
@@ -59,7 +59,7 @@ const checkInstructorObject = async (req, res, next) => {
         message: `An admin of id ${approved_by} was not found. Change approved_by to null or replace the value with a valid admin id.`,
       });
   }
-  const profile = await findById(profile_id);
+  const profile = await findBy({ profile_id });
   if (!profile)
     next({
       status: 400,
