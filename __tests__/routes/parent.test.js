1e845330fda09833fbdc2fa05277fa6e97f62275
CamMeiners <cammeiners@gmail.com>
2022-02-03 13:44:28 -0700

    removed extra code

--- __tests__/routes/parent.test.js
+++ __tests__/routes/parent.test.js
@@ -44,29 +44,6 @@ describe('parents router endpoints', () => {
     it('should return 404 when no user found', async () => {
       Parents.findByParentId.mockResolvedValue();
       const res = await request(server).get('/parents/1234123412341234');
-
-      expect(res.status).toBe(404);
-      expect(res.body.error).toBe('parentNotFound');
-    });
-  });
-  describe('GET /parents/:id/children', () => {
-    it('should return 200 when profile found', async () => {
-      Parents.findByParentId.mockResolvedValue({
-        id: '40',
-        name: 'Test Test',
-        email: 'test@test.com',
-      });
-      const res = await request(server).get('/parents/40');
-
-      expect(res.status).toBe(200);
-      expect(res.body.name).toBe('Test Test');
-      expect(Parents.getParents.mock.calls.length).toBe(1);
-    });
-
-    it('should return 404 when no user found', async () => {
-      Parents.findByParentId.mockResolvedValue();
-      const res = await request(server).get('/parents/1234123412341234');
-
       expect(res.status).toBe(404);
       expect(res.body.error).toBe('parentNotFound');
     });
