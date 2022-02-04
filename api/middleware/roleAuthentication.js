14361c927d211106e6853019af646d82f996b753
RoseGabriely <rosegabriely14@gmail.com>
2022-02-04 01:12:22 -0500

    removes async word causing router to break

--- api/middleware/roleAuthentication.js
+++ api/middleware/roleAuthentication.js
@@ -1,6 +1,6 @@
 const { getRoleById } = require('../roles/rolesModel');
 
-const roleAuthentication = async (...args) => (req, res, next) => {
+const roleAuthentication = (...args) => (req, res, next) => {
   req.profile.role = getRoleById(req.profile.role_id);
   const { role } = req.profile;
   if ([...args].includes(role)) {
