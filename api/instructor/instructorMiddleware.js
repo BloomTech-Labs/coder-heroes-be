2cd80bb6a52dd2095f2c578a71ad8706b52b7f33
Garrett Yien <yien.garrett94@gmail.com>
2022-02-07 15:47:38 -0500

    removed get, get by id, add, remove, update functions from model and router. these will be doable from profile router/model. removed unused checkinstructorobject middleware, changed checkinstructor exists to take profile_id, not instructor_id

--- api/instructor/instructorMiddleware.js
+++ api/instructor/instructorMiddleware.js
@@ -1,15 +1,12 @@
 const Instructors = require('./instructorModel');
 
-const { findByAdminId } = require('../admin/adminModel');
-const { findById } = require('../profile/profileModel');
-
 const checkInstructorExist = async (req, res, next) => {
-  const instructor_id = req.params.instructor_id;
-  const foundInstructor = await Instructors.findByInstructorId(instructor_id);
+  const profile_id = req.params.profile_id;
+  const foundInstructor = await Instructors.findByInstructorId(profile_id);
   if (foundInstructor == undefined) {
     next({
       status: 404,
-      message: `Instructor with id ${instructor_id} is not found.`,
+      message: `Instructor with id ${profile_id} is not found.`,
     });
   } else {
     req.instructor = foundInstructor;
@@ -17,59 +14,6 @@ const checkInstructorExist = async (req, res, next) => {
   }
 };
 
-const checkInstructorObject = async (req, res, next) => {
-  const { rating, bio, profile_id, status, approved_by } = req.body;
-  if (!rating) next({ status: 400, message: 'rating is required' });
-  if (!bio) next({ status: 400, message: 'bio is required' });
-  if (!profile_id) next({ status: 400, message: 'profile_id is required' });
-  if (!status) next({ status: 400, message: 'status is required' });
-  if (typeof rating !== 'number')
-    next({ status: 400, message: 'rating must be of type number' });
-  if (typeof bio !== 'string')
-    next({ status: 400, message: 'bio must be of type string' });
-  if (typeof profile_id !== 'number')
-    next({ status: 400, message: 'profile_id must be of type number' });
-  if (typeof status !== 'string')
-    next({ status: 400, message: 'status must be of type string' });
-  if (rating < 0 || rating >= 2 ** 31)
-    next({
-      status: 400,
-      message: 'rating must not have an absolute value that exceeds 2^31',
-    });
-  if (bio.length > 255)
-    next({
-      status: 400,
-      message: 'the bio string must not exceed a length of 255 characters',
-    });
-  if (status.length > 255)
-    next({
-      status: 400,
-      message: 'the status string must not exceed a length of 255 characters',
-    });
-  if (approved_by) {
-    if (typeof approved_by !== 'number')
-      next({
-        status: 400,
-        message: 'approved_by must be of type number or null',
-      });
-    const admin = await findByAdminId(approved_by);
-    if (!admin)
-      next({
-        status: 400,
-        message: `An admin of id ${approved_by} was not found. Change approved_by to null or replace the value with a valid admin id.`,
-      });
-  }
-  const profile = await findById(profile_id);
-  if (!profile)
-    next({
-      status: 400,
-      message: `profile with id ${profile_id} does not exist`,
-    });
-
-  next();
-};
-
 module.exports = {
   checkInstructorExist,
-  checkInstructorObject,
 };
--- api/instructor/instructorRouter.js
+++ api/instructor/instructorRouter.js
@@ -2,41 +2,15 @@ const express = require('express');
 const authRequired = require('../middleware/authRequired');
 const Instructors = require('./instructorModel');
 const router = express.Router();
-const {
-  checkInstructorExist,
-  checkInstructorObject,
-} = require('./instructorMiddleware');
-
-router.get('/', authRequired, function (req, res) {
-  Instructors.getInstructors()
-    .then((instructorList) => {
-      res.status(200).json(instructorList);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: err.message });
-    });
-});
-
-router.get('/:instructor_id', authRequired, checkInstructorExist, function (
-  req,
-  res,
-  next
-) {
-  try {
-    res.status(200).json(req.instructor);
-  } catch (error) {
-    next(error);
-  }
-});
+const { checkInstructorExist } = require('./instructorMiddleware');
 
 router.get(
-  '/:instructor_id/classes',
+  '/:profile_id/classes',
   authRequired,
   checkInstructorExist,
   function (req, res, next) {
     try {
-      Instructors.findInstructorCourses(req.params.instructor_id).then(
+      Instructors.findInstructorCourses(req.params.profile_id).then(
         (classes) => {
           if (classes) {
             res.status(200).json(classes);
@@ -50,79 +24,6 @@ router.get(
     }
   }
 );
-
-router.post('/', checkInstructorObject, async (req, res) => {
-  const instructor = req.body;
-  if (instructor) {
-    const { profile_id } = instructor;
-    try {
-      await Instructors.findByProfileId(profile_id).then(async (user) => {
-        if (user.length === 0) {
-          await Instructors.addInstructor(instructor).then((inserted) =>
-            res
-              .status(200)
-              .json({ message: 'Instructor added.', instructor: inserted[0] })
-          );
-        } else {
-          res.status(400).json({ message: 'Instructor already exists.' });
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ message: e.message });
-    }
-  } else {
-    res.status(404).json({ message: 'Instructor details missing.' });
-  }
-});
-
-router.put('/', authRequired, checkInstructorObject, (req, res) => {
-  const instructor = req.body;
-  if (instructor) {
-    const { instructor_id } = instructor;
-    Instructors.findByInstructorId(instructor_id)
-      .then(
-        Instructors.updateInstructor(instructor_id, instructor)
-          .then((updated) => {
-            res.status(200).json({
-              message: `Instructor with id: ${instructor_id} updated`,
-              instructor: updated[0],
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: `Could not update instructor '${instructor_id}'`,
-              error: err.message,
-            });
-          })
-      )
-      .catch((err) => {
-        res.status(404).json({
-          message: `Could not find instructor '${instructor_id}'`,
-          error: err.message,
-        });
-      });
-  }
-});
-
-router.delete('/:instructor_id', authRequired, (req, res) => {
-  const instructor_id = req.params.instructor_id;
-  try {
-    Instructors.findByInstructorId(instructor_id).then((instructor) => {
-      Instructors.removeInstructor(instructor[0].instructor_id).then(() => {
-        res.status(200).json({
-          message: `Instructor with id:'${instructor_id}' was deleted.`,
-          instructor: instructor[0],
-        });
-      });
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: `Could not delete instructor with ID: ${instructor_id}`,
-      error: err.message,
-    });
-  }
-});
 
 router.use('*', errorhandler);
 //eslint-disable-next-line
