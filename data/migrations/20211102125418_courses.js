6933bb93e895604bf5cea6be2a70722dedb935fa
Kat <kyevsukov@gmail.com>
2022-02-21 16:46:02 -0500

    added knex cleaner and cleanup seed

--- data/migrations/20211102125418_courses.js
+++ data/migrations/20211102125418_courses.js
@@ -32,11 +32,29 @@ exports.up = (knex) => {
       table.date('start_date').notNullable();
       table.date('end_date').notNullable();
       table.string('location').notNullable();
+    })
+    .createTable('instructors_course_types', (table) => {
+      table.increments('instructors_course_types_id');
+      table
+        .integer('profile_id')
+        .unsigned()
+        .notNullable()
+        .references('profile_id')
+        .inTable('profiles')
+        .onDelete('CASCADE');
+      table
+        .integer('course_type_id')
+        .unsigned()
+        .notNullable()
+        .references('course_type_id')
+        .inTable('course_types')
+        .onDelete('CASCADE');
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists('instructors_course_types')
     .dropTableIfExists('classes')
     .dropTableIfExists('course_types');
 };
