6c342cdf8c55d33dd08d5ab16808d41beddb0f78
Kat <kyevsukov@gmail.com>
2022-02-22 12:31:39 -0500

    removed courses and course_type_instructors tables, added programs table

--- data/migrations/20211102125418_courses.js
+++ data/migrations/20211102125418_courses.js
@@ -1,10 +1,9 @@
 exports.up = (knex) => {
   return knex.schema
-    .createTable('course_types', function (table) {
-      table.increments('course_type_id');
-      table.text('description').notNullable();
-      table.string('subject').notNullable().unique();
-      table.specificType('prereqs', 'text ARRAY');
+    .createTable('programs', function (table) {
+      table.increments('program_id');
+      table.string('program_name').notNullable().unique();
+      table.text('program_description').notNullable();
     })
 
     .createTable('classes', (table) => {
@@ -21,40 +20,20 @@ exports.up = (knex) => {
         .onDelete('CASCADE');
 
       table
-        .integer('course_type_id')
+        .integer('program_id')
         .unsigned()
         .notNullable()
-        .references('course_type_id')
-        .inTable('course_types')
+        .references('program_id')
+        .inTable('programs')
         .onDelete('CASCADE');
       table.time('start_time').notNullable();
       table.time('end_time').notNullable();
       table.date('start_date').notNullable();
       table.date('end_date').notNullable();
       table.string('location').notNullable();
-    })
-    .createTable('instructors_course_types', (table) => {
-      table.increments('instructors_course_types_id');
-      table
-        .integer('profile_id')
-        .unsigned()
-        .notNullable()
-        .references('profile_id')
-        .inTable('profiles')
-        .onDelete('CASCADE');
-      table
-        .integer('course_type_id')
-        .unsigned()
-        .notNullable()
-        .references('course_type_id')
-        .inTable('course_types')
-        .onDelete('CASCADE');
     });
 };
 
 exports.down = function (knex) {
-  return knex.schema
-    .dropTableIfExists('instructors_course_types')
-    .dropTableIfExists('classes')
-    .dropTableIfExists('course_types');
+  return knex.schema.dropTableIfExists('classes').dropTableIfExists('programs');
 };
