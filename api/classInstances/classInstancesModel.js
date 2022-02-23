72fac6b9787e9c08a5591afbeec6219e896f5a9e
Kat <kyevsukov@gmail.com>
2022-02-23 15:16:14 -0500

    updated getAllClassInstances function

--- api/classInstances/classInstancesModel.js
+++ api/classInstances/classInstancesModel.js
@@ -1,20 +1,15 @@
 const db = require('../../data/db-config');
 
-//justin
 const getAllClassInstances = async () => {
-  return await db('classes')
-    .leftJoin(
-      'course_types',
-      'classes.course_type_id',
-      'course_types.course_type_id'
-    )
-    .leftJoin(
-      'instructors',
-      'classes.instructor_id',
-      'instructors.instructor_id'
-    )
-    .leftJoin('profiles', 'instructors.profile_id', 'profiles.profile_id');
+  return await db('classes as c')
+    .select('c.*', 'p.program_name', 'i.instructor_id')
+    .leftJoin('programs as p', 'p.program_id', 'c.program_id')
+    .leftJoin('instructors as i', 'c.instructor_id', 'i.instructor_id');
 };
+
+getAllClassInstances().then((res) => {
+  console.log(res);
+});
 
 const findByClassInstanceId = async (class_id) => {
   return db('classes')
