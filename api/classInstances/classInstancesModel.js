9488679c15626e5969daafdc6671758921215061
Kat <kyevsukov@gmail.com>
2022-02-23 15:18:50 -0500

    updated findByClassInstanceId fundtion

--- api/classInstances/classInstancesModel.js
+++ api/classInstances/classInstancesModel.js
@@ -7,25 +7,18 @@ const getAllClassInstances = async () => {
     .leftJoin('instructors as i', 'c.instructor_id', 'i.instructor_id');
 };
 
-getAllClassInstances().then((res) => {
+const findByClassInstanceId = async (class_id) => {
+  return await db('classes as c')
+    .select('c.*', 'p.program_name', 'i.instructor_id')
+    .leftJoin('programs as p', 'p.program_id', 'c.program_id')
+    .leftJoin('instructors as i', 'c.instructor_id', 'i.instructor_id')
+    .where('c.class_id', class_id)
+    .first();
+};
+
+findByClassInstanceId(1).then((res) => {
   console.log(res);
 });
-
-const findByClassInstanceId = async (class_id) => {
-  return db('classes')
-    .leftJoin(
-      'course_types',
-      'classes.course_type_id',
-      'course_types.course_type_id'
-    )
-    .leftJoin(
-      'instructors',
-      'classes.instructor_id',
-      'instructors.instructor_id'
-    )
-    .leftJoin('profiles', 'instructors.profile_id', 'profiles.profile_id')
-    .where('classes.class_id', class_id);
-};
 
 const addClassInstance = async (newClass) => {
   return await db('classes').insert(newClass).returning('*');
