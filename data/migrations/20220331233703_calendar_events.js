159cd1d052515442570b276f9cab2decbf99cd4d
John Merz <jmerz826@gmail.com>
2022-03-31 23:53:40 -0400

    calendar_events table built

--- data/migrations/20220331233703_calendar_events.js
+++ data/migrations/20220331233703_calendar_events.js
@@ -1,6 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable('calendar_events', (table) => {
     table.increments('event_id');
+    table.string('date').notNullable();
+    table.string('type').notNullable();
+    table.string('content').notNullable();
+    table.string('details');
   });
 };
 
