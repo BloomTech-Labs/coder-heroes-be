8a840c8394c6ea220dd2450db680e6804d6736c3
John Merz <jmerz826@gmail.com>
2022-04-01 01:10:36 -0400

    addCalendarEvent added in model

--- api/calendarEvents/calendarEventsModel.js
+++ api/calendarEvents/calendarEventsModel.js
@@ -4,6 +4,14 @@ const getAllCalendarEvents = () => {
   return db('calendar_events').select('*');
 };
 
+const addCalendarEvent = async (newCalendarEvent) => {
+  const [createdCalendarEvent] = await db('calendar_events')
+    .insert(newCalendarEvent)
+    .returning('*');
+  return createdCalendarEvent;
+};
+
 module.exports = {
   getAllCalendarEvents,
+  addCalendarEvent,
 };
