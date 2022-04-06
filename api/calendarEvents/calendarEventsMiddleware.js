0bdf2b92e43b3a85e30e814e343ffc1e5506635d
luisbryandr <luisbdominguezr@gmail.com>
2022-04-06 12:19:33 -0400

    added createError to middleware

--- api/calendarEvents/calendarEventsMiddleware.js
+++ api/calendarEvents/calendarEventsMiddleware.js
@@ -1,15 +1,12 @@
 const CalendarEvents = require('./calendarEventsModel');
-// const createError = require('http-errors');
+const createError = require('http-errors');
 
 const checkCalendarEventExists = async (req, res, next) => {
   const event_id = parseInt(req.params.event_id);
   try {
     const foundEvent = await CalendarEvents.findCalendarEventById(event_id);
     if (!foundEvent) {
-      next({
-        status: 404,
-        message: `Calendar Event with id ${event_id} is not found`,
-      });
+      next(createError(404, `Calendar Event with id ${event_id} is not found`));
     } else {
       req.calendarEvent = foundEvent;
       next();
