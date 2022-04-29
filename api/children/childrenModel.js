5f9133c1acab8f32894137e68f73ac971c462e88
David Conley <dconley1989@gmail.com>
2022-04-29 11:06:21 -0600

    refactored some of the code in get enrolled courses to make it more efficient and take up less space. Also changed the function so that the endpoint would get course name and the instructor name for the front-end. EduardoEscobar and I worked on this

--- api/children/childrenModel.js
+++ api/children/childrenModel.js
@@ -4,10 +4,10 @@ const getChildren = async () => {
   return await db('children');
 };
 
-const findByChildId = async (id) => {
+const findByChildId = async (child_id) => {
   return db('children')
     .join('profiles', 'children.profile_id', 'profiles.profile_id')
-    .where('child_id', id)
+    .where('child_id', child_id)
     .first();
 };
 
@@ -15,40 +15,20 @@ const addChild = async (child) => {
   return await db('children').insert(child).returning('*');
 };
 
-const getEnrolledCourses = async (id) => {
+const getEnrolledCourses = async (child_id) => {
   // receives error if the id of the course doesn't exist exist  this have to modified later on we need to make a middleware that checks fot the existing courses then runs this model'
   const enrollments = await db('children')
     .join('enrollments', 'children.child_id', 'enrollments.child_id')
-    .join(
-      'courses',
-      'enrollments.course_id',
-      'courses.course_id',
-      'courses.course_name'
-    )
-    .where('children.child_id', id);
+    .join('courses', 'enrollments.course_id', 'courses.course_id')
+    .join('profiles', 'profiles.profiles_id', 'instructors.profile_id')
+    .where('children.child_id', child_id)
+    .select('enrollments.*, courses.*', 'profiles.name as instructor_name');
+  const child = await findByChildId(child_id);
 
-  const enrolledCourses = {
-    profile_id: enrollments[0].profile_id,
-    username: enrollments[0].username,
-    age: enrollments[0].age,
-    parent_id: enrollments[0].parent_id,
-    enrolled_courses: enrollments.map((x) => {
-      const container = {};
-      container.course_id = x.course_id;
-      container.completed = x.completed;
-      container.course_size = x.size;
-      container.open_seats_remaining = x.open_seats_remaining;
-      container.instructor_id = x.instructor_id;
-      container.course_type_id = x.course_type_id;
-      container.start_time = x.start_time;
-      container.end_time = x.end_time;
-      container.start_date = x.start_date;
-      container.end_date = x.end_date;
-      container.location = x.location;
-      return container;
-    }),
+  return {
+    ...child,
+    enrollments,
   };
-  return enrolledCourses;
 };
 
 const addEnrolledCourse = async (course) => {
