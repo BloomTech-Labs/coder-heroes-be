1f3c69e5920bdb7647ff1ddf2efe20e519a32916
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-05-03 15:57:52 -0400

    removed the hard coded profile_id from the profile seed

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -18,56 +18,48 @@ let profiles = [...new Array(20)].map((i, idx) => ({
 */
 profiles = [
   {
-    profile_id: 1,
     email: 'llama001@maildrop.cc',
     name: 'Test001 User',
     okta_id: '00ulthapbErVUwVJy4x6',
     role_id: 1,
   },
   {
-    profile_id: 2,
     email: 'llama002@maildrop.cc',
     name: 'Test002 User',
     okta_id: '00ultwew80Onb2vOT4x6',
     role_id: 2,
   },
   {
-    profile_id: 3,
     email: 'llama003@maildrop.cc',
     name: 'Test003 User',
     okta_id: '00ultx74kMUmEW8054x6',
     role_id: 3,
   },
   {
-    profile_id: 4,
     email: 'llama004@maildrop.cc',
     name: 'Test004 User',
     okta_id: '00ultwqjtqt4VCcS24x6',
     role_id: 4,
   },
   {
-    profile_id: 5,
     email: 'llama005@maildrop.cc',
     name: 'Test005 User',
     okta_id: '00ultwz1n9ORpNFc04x6',
     role_id: 5,
   },
   {
-    profile_id: 6,
     email: 'llama006@maildrop.cc',
     name: 'Test006 User',
     okta_id: '00u13omswyZM1xVya4x7',
     role_id: 1,
   },
   {
-    profile_id: 7,
     email: 'llama007@maildrop.cc',
     name: 'Test007 User',
     okta_id: '00u13ol5x1kmKxVJU4x7',
     role_id: 2,
   },
   {
-    profile_id: 8,
     email: 'llama008@maildrop.cc',
     name: 'Test008 User',
     okta_id: '00u13oned0U8XP8Mb4x7',
