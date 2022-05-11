573635901ce1e4e4b9517a69f3c7fa3b14352473
WRWPhillips <42WPhillips42@gmail.com>
2022-05-11 15:06:27 -0500

    Added two model functions for use in future routes.
    - findByInstructorId returns the courses that belong to an
      instructor with a given ID.
    - findInstructorIdByEmail returns instructor id based on the email
      which will be contained within a JWT, important for class compo
      nents.

--- api/courses/coursesModel.js
+++ api/courses/coursesModel.js
@@ -16,6 +16,22 @@ const findByCourseId = async (course_id) => {
     .first();
 };
 
+const findByInstructorId = async (instructor_id) => {
+  return await db('courses as c')
+    .select('c.*', 'p.program_name', 'i.instructor_id')
+    .leftJoin('programs as p', 'p.program_id', 'c.program_id')
+    .leftJoin('instructors as i', 'c.instructor_id', 'i.instructor_id')
+    .where('c.instructor_id', instructor_id);
+};
+
+const findInstructorIdByEmail = async (instructor_email) => {
+  return await db('instructors as i')
+    .select('i.instructor_id')
+    .leftJoin('profiles as p', 'i.profile_id', 'p.profile_id')
+    .where('p.email', instructor_email)
+    .first();
+};
+
 const addCourse = async (newCourse) => {
   const [createdCourse] = await db('courses').insert(newCourse).returning('*');
   return createdCourse;
@@ -35,6 +51,8 @@ const removeCourse = async (course_id) => {
 module.exports = {
   getAllCourses,
   findByCourseId,
+  findByInstructorId,
+  findInstructorIdByEmail,
   addCourse,
   updateCourse,
   removeCourse,
