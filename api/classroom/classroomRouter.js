b9901c644379c7d258a357e6964c8773f0d52709
WRWPhillips <42WPhillips42@gmail.com>
2022-05-18 12:03:53 -0500

    All endpoints but one working
    -- Was using the wrong name for a model function
    -- Naming conventions for routes were causing issues so I
      refactored

--- api/classroom/classroomRouter.js
+++ api/classroom/classroomRouter.js
@@ -3,10 +3,10 @@ const authRequired = require('../middleware/authRequired');
 const Classroom = require('./classroomModel');
 const router = express.Router();
 
-router.get('/:course_id', authRequired, function (req, res) {
+router.get('/students/:course_id', authRequired, function (req, res) {
   const course_id = parseInt(req.params.course_id);
   console.log(`${course_id} called here`);
-  Classroom.getStudentsById(course_id)
+  Classroom.getStudentsByClassId(course_id)
     .then((students) => {
       console.log('here');
       res.status(200).json(students);
@@ -24,8 +24,9 @@ router.get('/badges', authRequired, function (req, res, next) {
     .catch(next);
 });
 
-router.get('/:child_id/badges', authRequired, function (req, res, next) {
-  Classroom.getBadgesByChildId(req.params.child_id)
+router.get('/badges/:child_id', authRequired, function (req, res, next) {
+  const child_id = parseInt(req.params.child_id);
+  Classroom.getBadgesByChildId(child_id)
     .then((badges) => {
       res.status(200).json(badges);
     })
