e05e3871cde1bdb3a2196c0921662d75b15d9cc2
WRWPhillips <42WPhillips42@gmail.com>
2022-05-26 16:55:05 -0500

    Maybe a better sonarcloud fix?

--- data/migrations/20220511215733_feedback_badges.js
+++ data/migrations/20220511215733_feedback_badges.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
     .createTable('feedback_badges', (table) => {
       table.increments('badge_id');
@@ -9,23 +9,23 @@ exports.up = function(knex) {
     .createTable('student_badges', (table) => {
       table.increments('student_badge_id');
       table
-      .integer('child_id')
-      .unsigned()
-      .notNullable()
-      .references('child_id')
-      .inTable('children')
-      .onDelete('CASCADE');
+        .integer('child_id')
+        .unsigned()
+        .notNullable()
+        .references('child_id')
+        .inTable('children')
+        .onDelete('CASCADE');
       table
-      .integer('badge_id')
-      .unsigned()
-      .notNullable()
-      .references('badge_id')
-      .inTable('feedback_badges')
-      .onDelete('CASCADE');
+        .integer('badge_id')
+        .unsigned()
+        .notNullable()
+        .references('badge_id')
+        .inTable('feedback_badges')
+        .onDelete('CASCADE');
     });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema
     .dropTableIfExists('student_badges')
     .dropTableIfExists('feedback_badges');
--- data/seeds/005_children.js
+++ data/seeds/005_children.js
@@ -1,5 +1,5 @@
 exports.seed = function (knex) {
   return knex('children').insert([
-    { parent_id: 1, profile_id: 5, username: 'ILoveFortnite', age: 8, },
+    { parent_id: 1, profile_id: 5, username: 'ILoveFortnite', age: 8 },
   ]);
 };
