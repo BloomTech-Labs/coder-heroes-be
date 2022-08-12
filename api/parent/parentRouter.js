8408b155254c6dcdc2b208091cebf6f5ba1f2876
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2022-08-12 15:02:19 -0600

    can now add child under the endpoint /parent

--- api/parent/parentRouter.js
+++ api/parent/parentRouter.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const authRequired = require('../middleware/authRequired');
 const Parents = require('./parentModel');
+const Children = require('../children/childrenModel');
 const router = express.Router();
+
+router.post('/', authRequired, async function (req, res, next) {
+  const { profile_id } = req.profile;
+  try {
+    let child = await Children.addChild(profile_id, req.body);
+    res.status(201).json(child);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:profile_id/children', authRequired, function (req, res) {
   const { profile_id } = req.params;
